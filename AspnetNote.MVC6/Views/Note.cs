03a56d48470ee8e740f407cf6cc2aca15e3b5c96
keonhee Yoon <keonhee00@gmail.com>
2021-01-12 21:16:25 +0900

    초기 커밋.

---
+++ AspnetNote.MVC6/Views/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.MVC6.Views
+{
+    public class Note
+    {
+        public int NoteNo { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteContents { get; set; }
+        public int UserNo { get; set; }
+    }
+}
---
+++ AspnetNote.MVC6/Views/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.MVC6.Views
+{
+    public class User
+    {
+        public int UserNo { get; set; }
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
