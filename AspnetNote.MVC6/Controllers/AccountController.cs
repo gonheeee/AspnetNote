90ff3e8e62853a1fd2a586f5cd09a9881cd8d6ba
keonhee Yoon <keonhee00@gmail.com>
2021-01-13 20:57:10 +0900

    8. 회원가입 기능 만들기

---
+++ AspnetNote.MVC6/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetNote.MVC6.DataContext;
+using AspnetNote.MVC6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspnetNote.MVC6.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+
+        /// <summary>
+        /// 회원 가입
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // 전송하는 Action Annotation 선언
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                using(var db = new AspnetNoteDbContext())
+                {
+                    db.Users.Add(model);
+                    db.SaveChanges(); // DB Commit
+                }
+                // Home 컨트롤러의 Index로 넘김
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
