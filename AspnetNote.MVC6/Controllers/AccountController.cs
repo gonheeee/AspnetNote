d5901c036372098686269fdd4124ba9f9576c916
keonhee Yoon <keonhee00@gmail.com>
2021-01-17 16:12:41 +0900

    10. 로그인 입력 Form 만들기

--- AspnetNote.MVC6/Controllers/AccountController.cs
+++ AspnetNote.MVC6/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspnetNote.MVC6.DataContext;
 using AspnetNote.MVC6.Models;
+using AspnetNote.MVC6.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,9 +17,32 @@ namespace AspnetNote.MVC6.Controllers
         /// 로그인
         /// </summary>
         /// <returns></returns>
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    // Linq Method Chaining
+                    var user = db.Users.FirstOrDefault(u => u.UserId.Equals(model.UserId) && 
+                                                            u.UserPassword.Equals(model.UserPassword));
+                    if(user != null)
+                    {
+                        // Login Success
+                        return RedirectToAction("LoginSuccess", "Home");   
+                    }                    
+                }
+                // Login Fail
+                ModelState.AddModelError(string.Empty, "Invalid User");
+            }
+            return View(model);
         }
         
 
@@ -31,6 +55,11 @@ namespace AspnetNote.MVC6.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 회원가입 전송
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         // 전송하는 Action Annotation 선언
         [HttpPost]
         public IActionResult Register(User model)
