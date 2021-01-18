3193fe999b2188084a11fe1b3092e3bb1c33f832
keonhee Yoon <keonhee00@gmail.com>
2021-01-18 20:22:58 +0900

    12. 게시판 리스트 출력하기 13. 게시물 추가하기

---
+++ AspnetNote.MVC6/Controllers/NoteController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetNote.MVC6.DataContext;
+using AspnetNote.MVC6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspnetNote.MVC6.Controllers
+{
+    public class NoteController : Controller
+    {
+        /// <summary>
+        /// 게시판 리스트 출력
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 아닌 상태
+                return RedirectToAction("Login", "Account");
+            }
+            using (var db = new AspnetNoteDbContext())
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+        }
+
+        /// <summary>
+        /// 게시물 추가
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 아닌 상태
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Note model)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 아닌 상태
+                return RedirectToAction("Login", "Account");
+            }
+
+            model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
+
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    db.Notes.Add(model);
+                    
+                    if(db.SaveChanges() > 0)
+                        return Redirect("Index");
+                    // 동일한 컨트롤러 내에서는 Redirect, 타 컨트롤러에 접근할때는 RedirectToAction("Action", "Controller")
+
+                }
+                ModelState.AddModelError(string.Empty, "게시물을 저장할 수 없습니다.");
+            }
+            return View(model);
+        }
+
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 아닌 상태
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Delete()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 아닌 상태
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+    }
+}
